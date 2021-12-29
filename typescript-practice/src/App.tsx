fee0267ea9a37a71737bc34b5d4e89568c1a7d9e
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-29 12:47:22 -0500

    adding data model

--- typescript-practice/src/App.tsx
+++ typescript-practice/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Land new nursing job'),
+    new Todo('Take a nap')
+
+  ];
+
   return (
     <div>
-      <Todos items={['Land new job', 'Take a nap']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
