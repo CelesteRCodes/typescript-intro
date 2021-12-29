b93e186beeb79a522eab4dae746feb15ddfa057f
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-29 11:06:17 -0500

    initial

---
+++ basics.ts
@@ -0,0 +1,19 @@
+// primitives: numbers, strings, booleans
+// complex types: arrays, objects
+// function types, parameters
+
+// primitives (start in lowercase): 
+let age: number;
+// let age: number = 22;
+age = 12; 
+
+let userName: string;
+userName = 'Celeste'
+
+let isFinanciallyAbundant: boolean;
+isFinanciallyAbundant = true;
+
+// don't set things to null, can't reassign 
+let hobbies: null;
+// hobbies = 'sleep' (not assignable to 'null')
+
